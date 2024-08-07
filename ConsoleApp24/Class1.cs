884ff42300a97f1fd853410e66af31ba87c2bf04
Rohat Güneş <rohatgns44@gmail.com>
2024-08-07 17:33:20 +1000

    Add project files.

---
+++ ConsoleApp24/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    public class Bebek 
+    {
+        public DateTime DogumTarihi { get; set; }
+        public string Ad { get; set; }
+
+        public string SoyAd { get; set; }
+
+        // bu kısımda deger verilmedği için kendi degerimizi  gecerli sayacagız ve default 
+        // constructor kullanacagız 
+        public Bebek()
+        {
+            DogumTarihi = DateTime.Now;
+            Console.WriteLine("Ingaaa");
+            //burda tam zamanlı gerı dönmeyen bir bebek nesnesi oluşturduk 
+        }
+         //şimdi ise bir paramtere alan constructor olulşturalım  bebek nesensi oluşturacagız 
+
+        public  Bebek(string ad,string soyad)
+        {
+            DogumTarihi = DateTime.Now;
+            Ad = ad;
+            SoyAd = soyad;
+            Console.WriteLine("Ingaaa");
+
+
+
+        }
+
+    }
+}
---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleApp24;
+using System;
+
+public class Program
+{
+
+    public static void Main()
+    {
+        Bebek bebek1 = new Bebek();
+        Console.WriteLine("Bebek 1:");
+        Console.WriteLine($"Ad :{bebek1.Ad}");
+        Console.WriteLine($"Soyad :{bebek1.SoyAd}");
+        Console.WriteLine($"Dogum tarihi :{bebek1.DogumTarihi}");
+
+        //paremetleri constructor ile bebek nesnesi oluşturmamız gerek şimdi de 
+
+        Bebek bebek2 = new Bebek("Ali","Veli");
+        Console.WriteLine("\nBebek 2: ");
+        Console.WriteLine($"Ad:{bebek2.Ad}");
+        Console.WriteLine($"Soyad:{bebek2.SoyAd}");
+        Console.WriteLine($"dogum tarihi :{bebek2.DogumTarihi}");
+    } 
+
+
+}
